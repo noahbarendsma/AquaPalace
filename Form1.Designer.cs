8a231090cfd7995876f2fa92f2d7c7558ced0658
Noah Barendsma <nbarendsma@gmail.com>
2025-09-12 11:48:17 +0200

    Frontend login scherm met kleur en plaatje

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,51 +32,84 @@
             this.txtWachtwoord = new System.Windows.Forms.TextBox();
             this.lblGebruikersnaam = new System.Windows.Forms.Label();
             this.lblWachtwoord = new System.Windows.Forms.Label();
+            this.btnInloggen = new System.Windows.Forms.Button();
+            this.pbxInlog = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxInlog)).BeginInit();
             this.SuspendLayout();
             // 
             // txtGebruikersnaam
             // 
-            this.txtGebruikersnaam.Location = new System.Drawing.Point(369, 96);
+            this.txtGebruikersnaam.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGebruikersnaam.Location = new System.Drawing.Point(259, 96);
             this.txtGebruikersnaam.Name = "txtGebruikersnaam";
-            this.txtGebruikersnaam.Size = new System.Drawing.Size(100, 22);
+            this.txtGebruikersnaam.Size = new System.Drawing.Size(210, 22);
             this.txtGebruikersnaam.TabIndex = 0;
             // 
             // txtWachtwoord
             // 
-            this.txtWachtwoord.Location = new System.Drawing.Point(369, 160);
+            this.txtWachtwoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWachtwoord.Location = new System.Drawing.Point(259, 160);
             this.txtWachtwoord.Name = "txtWachtwoord";
-            this.txtWachtwoord.Size = new System.Drawing.Size(100, 22);
+            this.txtWachtwoord.PasswordChar = '*';
+            this.txtWachtwoord.Size = new System.Drawing.Size(210, 22);
             this.txtWachtwoord.TabIndex = 1;
             // 
             // lblGebruikersnaam
             // 
             this.lblGebruikersnaam.AutoSize = true;
-            this.lblGebruikersnaam.Location = new System.Drawing.Point(256, 99);
+            this.lblGebruikersnaam.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGebruikersnaam.Location = new System.Drawing.Point(256, 74);
             this.lblGebruikersnaam.Name = "lblGebruikersnaam";
-            this.lblGebruikersnaam.Size = new System.Drawing.Size(107, 16);
+            this.lblGebruikersnaam.Size = new System.Drawing.Size(125, 16);
             this.lblGebruikersnaam.TabIndex = 2;
-            this.lblGebruikersnaam.Text = "Gebruikersnaam";
+            this.lblGebruikersnaam.Text = "Gebruikersnaam:";
             // 
             // lblWachtwoord
             // 
             this.lblWachtwoord.AutoSize = true;
-            this.lblWachtwoord.Location = new System.Drawing.Point(256, 163);
+            this.lblWachtwoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWachtwoord.Location = new System.Drawing.Point(256, 138);
             this.lblWachtwoord.Name = "lblWachtwoord";
-            this.lblWachtwoord.Size = new System.Drawing.Size(82, 16);
+            this.lblWachtwoord.Size = new System.Drawing.Size(96, 16);
             this.lblWachtwoord.TabIndex = 3;
-            this.lblWachtwoord.Text = "Wachtwoord";
+            this.lblWachtwoord.Text = "Wachtwoord:";
+            // 
+            // btnInloggen
+            // 
+            this.btnInloggen.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnInloggen.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInloggen.ForeColor = System.Drawing.Color.White;
+            this.btnInloggen.Location = new System.Drawing.Point(259, 199);
+            this.btnInloggen.Name = "btnInloggen";
+            this.btnInloggen.Size = new System.Drawing.Size(210, 39);
+            this.btnInloggen.TabIndex = 4;
+            this.btnInloggen.Text = "Inloggen";
+            this.btnInloggen.UseVisualStyleBackColor = false;
+            // 
+            // pbxInlog
+            // 
+            this.pbxInlog.BackgroundImage = global::AquaPalace.Properties.Resources.ChatGPT_Image_12_sep_2025__11_46_31;
+            this.pbxInlog.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxInlog.Location = new System.Drawing.Point(1, -1);
+            this.pbxInlog.Name = "pbxInlog";
+            this.pbxInlog.Size = new System.Drawing.Size(252, 258);
+            this.pbxInlog.TabIndex = 5;
+            this.pbxInlog.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(496, 257);
+            this.Controls.Add(this.pbxInlog);
+            this.Controls.Add(this.btnInloggen);
             this.Controls.Add(this.lblWachtwoord);
             this.Controls.Add(this.lblGebruikersnaam);
             this.Controls.Add(this.txtWachtwoord);
             this.Controls.Add(this.txtGebruikersnaam);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxInlog)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +121,8 @@
         private System.Windows.Forms.TextBox txtWachtwoord;
         private System.Windows.Forms.Label lblGebruikersnaam;
         private System.Windows.Forms.Label lblWachtwoord;
+        private System.Windows.Forms.Button btnInloggen;
+        private System.Windows.Forms.PictureBox pbxInlog;
     }
 }
 
